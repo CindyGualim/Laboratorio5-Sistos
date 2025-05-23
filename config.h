c562263ce4be138a9681945b81d311900a34ec9e
CindyGualim <cindy@DESKTOP-565G713.>
2025-05-22 20:03:14 -0600

    Agrego archivo de configuración inicial

---
+++ config.h
@@ -0,0 +1,17 @@
+// config.h
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Número de recursos disponibles al inicio
+#define N_RECURSOS 5
+
+// Número total de hilos que se lanzarán
+#define N_THREADS 10
+
+// Cantidad de iteraciones que hace cada hilo
+#define N_ITER 20
+
+// Máxima cantidad de recursos que puede pedir un hilo (para el monitor)
+#define MAX_PETICION 3
+
+#endif
